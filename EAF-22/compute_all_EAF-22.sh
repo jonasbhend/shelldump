cdad297bcd51fa1666a31be9b9329a0de2ba7718
Jonas Bhend <jonas.bhend@gmx.ch>
2016-01-11 10:47:38 +0100

    Complete list of simulations available

--- EAF-22/compute_all_EAF-22.sh
+++ EAF-22/compute_all_EAF-22.sh
@@ -5,9 +5,9 @@
 
 cd $HOME/logs
 
-for method in fastqqmap ; do
-  for model in ecmwf-system4 SMHI-EC-EARTH SMHI-RCA4 UCAN-WRF341G UL-IDL-WRF360D DWD-CCLM4-8-21 ; do
-      for varname in tasmin tasmax pr ; do
+for method in fastqqmap none ; do
+  for model in SMHI-EC-EARTH SMHI-RCA4 UCAN-WRF341G UL-IDL-WRF360D DWD-CCLM4-8-21 ecmwf-system4 ; do
+      for varname in tasmin tasmax pr ept wb ; do
           master_skill_scores.sh -v $model WFDEI $varname EAF-22 $method 05
       done
   done
