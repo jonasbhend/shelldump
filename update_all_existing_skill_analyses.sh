66d764851741001856710df9291933f8aeb560d3
Jonas Bhend <jonas.bhend@gmx.ch>
2016-01-21 17:32:22 +0100

    New script to update all existing skill analyses

---
+++ update_all_existing_skill_analyses.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+targets=$( \ls -d /store/msclim/bhendj/EUPORIAS/*/*/daily/*/* )
+
+for target in $targets ; do
+    if [[ ! -f $target ]] ; then
+        model=$( echo $target | sed -e 's_.*EUPORIAS/__g' -e 's_/_ _g' | awk '{ print $1 }' )
+        grid=$( echo $target | sed -e 's_.*EUPORIAS/__g' -e 's_/_ _g' | awk '{ print $2 }' )
+        varname=$( echo $target | sed -e 's_.*EUPORIAS/__g' -e 's_/_ _g' | awk '{ print $4 }' )
+        method=$( echo $target | sed -e 's_.*EUPORIAS/__g' -e 's_/_ _g' | awk '{ print $5 }' | sed 's/_.*//g' )
+        obs=$( echo $target | sed -e 's_.*EUPORIAS/__g' -e 's_/_ _g' | awk '{ print $5 }' | sed 's/.*_//g' )
+        files=$( \ls $target/*.nc )
+        if [[ $files != "" && ! $method =~ "none" ]] ; then
+            inits=""
+            for f in $files ; do
+                fi=$( basename $f )
+                init=$( echo $fi | cut -c 5-6 )
+                if [[ ! $inits =~ $init ]] ; then
+                    inits="$inits $init"
+                fi
+            done
+
+            for init in $inits ; do
+                echo $model $obs $varname $grid $method $init
+                master_skill_scores.sh -v $model $obs $varname $grid $method $init
+            done
+        fi
+    fi
+done
