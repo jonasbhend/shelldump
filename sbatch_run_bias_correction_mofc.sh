d8282f183a4d5c9d9972fd7350b6be760c5d41ce
Jonas Bhend <jonas.bhend@gmx.ch>
2016-02-01 11:46:42 +0100

    run bias correction and skill for MOFC

---
+++ sbatch_run_bias_correction_mofc.sh
@@ -0,0 +1,55 @@
+#!/usr/local/bin/bash
+
+grid=eobs0.44
+dpath=/store/msclim/bhendj/MOFC/$grid
+
+## enquire current directory
+curdir=`pwd`
+TMPDIR=$SCRATCH/sbatch_files
+mkdir -p $TMPDIR
+
+
+## loop through variable name and debiasing options
+for  granul in weekly ; do
+    for varname in tas  ; do
+        files=$(\ls $dpath/$granul/$varname/none/*.nc )
+        for method in unbias ; do
+            for file in $files ; do
+                
+                batchfile=$TMPDIR/${varname}_${method}_${RANDOM}.batch
+                echo $varname $method $file $batchfile
+                
+            ## write the batch file
+                cat > $batchfile <<EOF
+#!/usr/local/bin/bash
+
+#SBATCH --mail-user=jonas.bhend@meteoswiss.ch
+#SBATCH --job-name=mofc_bias
+#SBATCH --mail-type=FAIL
+#SBATCH --time=04:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=18GB
+#SBATCH --partition=postproc
+#SBATCH --output=$HOME/logs/mofc_bias_${varname}_${method}_%j.log
+
+#======START=====
+srun Rscript /users/bhendj/R/sbatch_bias_correct_mofc.R $file $method
+
+EOF
+                
+                sbatch $batchfile
+                rm $batchfile
+                
+            done
+        done
+    done
+done
+
+rm -rf $TMPDIR
+
+exit
+
+# 
+# sbatch_run_bias_correction_mofc.sh ends here
---
+++ sbatch_run_scores_mofc.sh
@@ -0,0 +1,59 @@
+#!/usr/local/bin/bash
+
+grid=eobs0.44
+dpath=/store/msclim/bhendj/MOFC/$grid
+
+## enquire current directory
+curdir=`pwd`
+TMPDIR=$SCRATCH/sbatch_files
+mkdir -p $TMPDIR
+
+
+## loop through variable name and debiasing options
+for granul in weekly ; do
+    for varname in tas  ; do
+        for method in none smooth-crossval1 ; do
+            files=$(\ls $dpath/$granul/$varname/${method}*/*.nc )
+            for file in $files ; do
+                
+                outdir=$( dirname $file | sed "s/MOFC/MOFC_scores/g" )
+                outfile=scores_$( basename $file )
+                
+                if [[ ! -f $outdir/$outfile ]] ; then
+                    batchfile=$TMPDIR/scores_${varname}_${method}_${RANDOM}.batch
+                ## echo $varname $method $file $batchfile
+                    
+                ## write the batch file
+                    cat > $batchfile <<EOF
+#!/usr/local/bin/bash
+
+#SBATCH --mail-user=jonas.bhend@meteoswiss.ch
+#SBATCH --job-name=mofc_scores
+#SBATCH --mail-type=FAIL
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=18GB
+#SBATCH --partition=postproc
+#SBATCH --output=$HOME/logs/mofc_scores_${varname}_${method}_%j.log
+
+#======START=====
+srun Rscript /users/bhendj/R/sbatch_compute_skill_mofc.R $file
+
+EOF
+
+                    sbatch $batchfile
+                    rm $batchfile
+                fi
+            done
+        done
+    done
+done
+
+rm -rf $TMPDIR
+
+exit
+
+# 
+# sbatch_run_bias_correction_mofc.sh ends here
