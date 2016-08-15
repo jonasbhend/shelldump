35ec9e3de7ba0989aab554386ba6bb6a0ce05e17
Jonas Bhend <jonas.bhend@gmx.ch>
2016-08-15 08:02:47 +0200

    NULL

--- compute_virtual_threshold_from_obs.sh
+++ compute_virtual_threshold_from_obs.sh
@@ -40,8 +40,8 @@ for obs in ERA-INT ; do
             cdo -s ydrunmin,$ndays $SCRATCH/$ofile $SCRATCH/timmin.$ofile
             cdo -s ydrunmax,$ndays $SCRATCH/$ofile $SCRATCH/timmax.$ofile
 
-            ##for pctl in 01 02 05 10 20 30 40 50 60 70 80 90 95 98 99 ; do
-            for pctl in 25 75 ; do
+            for pctl in 01 02 05 10 20 30 40 50 60 70 80 90 95 98 99 ; do
+            ##for pctl in 25 75 ; do
                 echo $pctl
                 cdo -s ydrunpctl,$pctl,$ndays $SCRATCH/$ofile $SCRATCH/timmin.$ofile $SCRATCH/timmax.$ofile $outdir/pctl${pctl}_$ofile 
                 ## Rscript /users/bhendj/R/compute_seasonal_percentiles.R $SCRATCH/$ofile $pctl $outdir/seaspctl${pctl}_$ofile
